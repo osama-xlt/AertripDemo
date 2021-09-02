03c0bc326a885b778c6511c8f8ec8822ca6928f3
osama-xlt <osama_xlt@hotmail.com>
2021-09-02 19:28:23 +0530

    Initial Commit

---
+++ AertripDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AertripDemo
+//
+//  Created by Osama Mohammed Shaikh on 02/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
